d2107e98ff0e8b45817725c9616db434e738ea9b
Maxhyt <Maxhyt@users.noreply.github.com>
2018-04-09 16:31:18 +1200

    Update 09/04/2018 - v2.1.2

--- InstagramPlusPlus.meta.js
+++ InstagramPlusPlus.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram++
 // @namespace    maxhyt.instagrampp
-// @version      2.1.1.1
+// @version      2.1.2
 // @updateURL    https://maxhyt.github.io/InstagramPlusPlus/InstagramPlusPlus.meta.js
 // @downloadURL  https://maxhyt.github.io/InstagramPlusPlus/InstagramPlusPlus.user.js
 // ==/UserScript==
