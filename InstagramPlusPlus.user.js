6798d02820368668d745fc42503e76c3093d9965
Maxhyt <Maxhyt@users.noreply.github.com>
2018-04-09 15:05:57 +1200

    Update InstagramPlusPlus.user.js

--- InstagramPlusPlus.user.js
+++ InstagramPlusPlus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram++
 // @namespace    maxhyt.instagrampp
-// @version      2.1.1.1
+// @version      2.1.1.2
 // @description  Instagram++ Help Tools
 // @author       Maxhyt
 // @homepage     https://maxhyt.github.io/InstagramPlusPlus/
